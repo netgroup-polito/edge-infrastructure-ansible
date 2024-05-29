418817a3f8da6d81c3f0c437a1fe8acfa7a3b6c8
Giovanni Mirarchi <84398132+giovannimirarchi420@users.noreply.github.com>
2024-05-29 14:08:28 +0200

    Feature/ask grafana psw (#20)
    * Ask Grafana password to the user, readme update
    
    * readme update
    
    * password change message fix
    
    * README.md headlines update

--- setup/edge-pc-local-setup.sh
+++ setup/edge-pc-local-setup.sh
@@ -10,6 +10,137 @@ cat << "EOF"
 EOF
 
 #######################################
+#    UTILITY FUNCTIONS DEFINITION     #
+#######################################
+
+function match_string() {
+  if [ "$1" != "$2" ]; then
+    echo "Sorry, passwords do not match. Try again"
+    return 1
+  fi
+}
+
+function check_passwords() {
+  while true; do
+    read -p "$1" answer
+    case $answer in
+      [Yy])
+        CHECK_OK=0
+        while true; do
+          read -rep "New password: " -s pw1
+
+          read -rep "Retype new password: " -s pw2
+
+          match_string "$pw1" "$pw2"
+          if [ $? -eq 0 ]; then
+            CHECK_OK=1
+            if [ "$2" == "grafana" ]; then
+              GRAFANA_PSW="$pw1"
+            else
+              PSW_MGMT="$pw1"
+            fi
+            break
+          fi
+        done
+        if [ $CHECK_OK -eq 1 ]; then
+          echo "Password changed successfully"
+          return 0
+          break
+        fi
+        ;;
+      [Nn])
+        break
+        ;;
+      *)
+        echo "Please provide a valid answer (y/n)"
+        ;;
+    esac
+  done
+
+}
+
+
+####################################################
+#                                                  #
+# These variables and functions are used to        #
+# construct a summary table at the end of          #
+# the script execution.                            #
+#                                                  #
+####################################################
+column_len=( 13 40 20 )
+table_vertical_separator="|"
+table_horizontal_separator="-"
+table_border_mark="+"
+
+
+write_separate_line() {
+        for n in "${column_len[@]}"
+        do
+        echo -n $table_border_mark
+        for i in $(seq 1 $n); do echo -n $table_horizontal_separator; done
+        done
+        echo -n $table_border_mark
+        echo ""
+}
+
+write_row() {
+        printed=""
+        recurse=""
+        remaining_cols=()
+        ci=0
+        echo -n $table_vertical_separator
+        while (( "$#" )); do
+                value=$1
+                remaining=${column_len[ci]}
+                size=${#value}
+                substr=""
+                if [ "$size" -gt "$remaining" ]; then
+                    let "start=remaining+1"
+                    substr=$(echo "$value" | cut -c$start-$size)
+                    value=$(echo "$value" | cut -c1-$remaining)
+                    recurse="yes"
+                fi
+                remaining_cols+=($substr)
+                echo -n $value
+                let "remaining-=size"
+                for i in $(seq 1 $remaining); do echo -n " "; done
+                echo -n $table_vertical_separator
+                let "ci+=1"
+                shift
+        done
+
+        while [ $ci -lt "${#column_len[@]}" ]
+        do
+            remaining=${column_len[ci]}
+            for i in $(seq 1 $remaining); do echo -n " "; done
+            echo -n $table_vertical_separator
+            let "ci+=1"
+        done
+
+        echo ""
+        if [ ! -z "$recurse" ]
+        then
+        write_row "${remaining_cols[@]}"
+        fi
+
+}
+
+print_end_message() {
+        write_separate_line
+        write_row "Dashboard" "URL" "Password"
+        write_separate_line
+        write_row "General" "http://$1/" "Not required"
+        write_separate_line
+        write_row "K3S" "http://$1/k3sdashboard" "Check README.md"
+        write_separate_line
+        write_row "Grafana" "http://$1/grafana" "$2"
+        write_separate_line
+        write_row "Prometheus" "http://$1/prometheus/graph" "Not required"
+        write_separate_line
+}
+
+
+#######################################
 #   STEP 0: CHECK SUDO PRIVILEGES     #
 #######################################
 
@@ -47,6 +178,11 @@ if [ ! "$ID" == "ubuntu" ]; then
     echo
     exit 1
 fi
+
+GRAFANA_PSW_ASK_STRING="Would you like to change the Grafana password for 'admin' user?
+Pressing 'n' will set the password to the default 'root' (y/n): "
+GRAFANA_PSW="root"
+check_passwords "$GRAFANA_PSW_ASK_STRING" "grafana"
 
 # Create mgmt user
 echo -n "Management user creation..."
@@ -128,8 +264,10 @@ if [ $# -eq 3 ]; then
   sed -i "s/<remote_target_password>/$3/g" /home/mgmt/edge-infrastructure-ansible-main/inventory
   sed -i "s/<remote_target_ip>/$1/g" /home/mgmt/edge-infrastructure-ansible-main/playbook/roles/liqo-get-kubeconfig-remote/vars/main.yaml
   sed -i "s/<remote_target_user>/$2/g" /home/mgmt/edge-infrastructure-ansible-main/playbook/roles/liqo-get-kubeconfig-remote/tasks/main.yaml
+  sed -i "s/<grafana_password>/$GRAFANA_PSW/g" /home/mgmt/edge-infrastructure-ansible-main/playbook/roles/energymon/files/values.yaml
 else
   sed -i "s/^.*<remote_target_ip>/#&/g" /home/mgmt/edge-infrastructure-ansible-main/inventory
+  sed -i "s/<grafana_password>/$GRAFANA_PSW/g" /home/mgmt/edge-infrastructure-ansible-main/playbook/roles/energymon/files/values.yaml
 fi
 
 
@@ -174,46 +312,20 @@ done
 ################################################
 #          STEP 7: Password change             #
 ################################################
-function check_passwords() {
-  if [ $1 != $2 ]; then
-    echo "Sorry, passwords do not match. Try again"
-    return 1
-  fi
-}
+MGMT_USER_PSW_ASK_STRING="The user 'mgmt' has been created and used for the installation.
+The default password is: root
+Would you like to change the password? (y/n) "
+PSW_MGMT="root"
+check_passwords "$MGMT_USER_PSW_ASK_STRING" "mgmt"
+echo "mgmt:$PSW_MGMT" | chpasswd
 
-while true; do
-  read -p "The user mgmt was been created and used to perform the installation.
-  The default password is: root
-  Would you like to change the password? (y/n) " answer
-  case $answer in
-    [Yy])
-      CHECK_OK=0
-      while true; do
-        read -p "New password: " -s pw1
-        echo
-        read -p "Retype new password: " -s pw2
-        echo
+if [ $? -ne 0 ]; then
+  echo "There was an error while changing the password. As a result, the default password 'root' will remain in place."
+fi
+echo ""
 
-        check_passwords "$pw1" "$pw2"
-        if [ $? -eq 0 ]; then
-          CHECK_OK=1
-          break
-        fi
-      done
-      if [ $CHECK_OK -eq 1 ]; then
-        echo "mgmt:$pw1" | chpasswd
-        echo "Password changed successfully."
-        break
-      fi
-      ;;
-    [Nn])
-      break
-      ;;
-    *)
-      echo "Please provide a valid answer (y/n)"
-      ;;
-  esac
-done
-
+# Get local IP address
+local_ip=$(hostname -I | awk '{print $1}')
+print_end_message "$local_ip" "$GRAFANA_PSW"
+echo ""
 echo "Installation completed."
-exit 0
